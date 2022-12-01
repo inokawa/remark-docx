6fb7dba7631ae51376507edb9fd2c32d8a5cc337
inokawa <48897392+inokawa@users.noreply.github.com>
2022-12-01 10:05:02 +0900

    Fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,8 +3,11 @@ module.exports = {
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: ["@storybook/addon-essentials"],
   framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
   webpackFinal: async (config, { configType }) => {
     config.module.rules.push({
       test: /\.js$/,
