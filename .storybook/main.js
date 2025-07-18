9fabf1223b15e3d60dc3ca738f13d8e1b504d14b
inokawa <48897392+inokawa@users.noreply.github.com>
2025-07-18 12:52:54 +0900

    Remove @storybook/addon-docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,6 @@ import { mergeConfig } from "vite";
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 export default {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-docs"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
